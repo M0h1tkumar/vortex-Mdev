616e1a110d4b36aa7dddfd9288ab1668cfc46708
Anubhav Jaiswal <anubhavjaiswal2002@gmail.com>
2026-05-01 00:49:55 +0530

    feat: remove SQLite adapter from PrismaClient configuration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,5 @@
 import pkg from '@prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-import { env } from './env.js';
 
 const { PrismaClient } = pkg;
 
-const adapter = new PrismaBetterSqlite3({ url: env.DATABASE_URL });
-
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
