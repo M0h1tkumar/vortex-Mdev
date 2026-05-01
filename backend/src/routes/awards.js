b06680b88ad05ddb37b8ab032d88aaa5f4d14002
Anubhav Jaiswal <anubhavjaiswal2002@gmail.com>
2026-05-01 12:25:54 +0530

    feat: simplify awards route by removing unused endpoints

--- backend/src/routes/awards.js
+++ backend/src/routes/awards.js
@@ -1,25 +1,8 @@
 import { Router } from 'express';
-import {
-  updateGlobalConfig,
-  getGlobalConfig,
-  broadcastMail,
-  generateReport
-} from '../controllers/admin.controller.js';
-import {
-  createInstitute,
-  getInstitutes,
-  deleteInstitute
-} from '../controllers/domains.controller.js';
+import { getAwards } from '../controllers/awards.controller.js';
 
 const router = Router();
 
-router.get('/config', getGlobalConfig);
-router.patch('/config', updateGlobalConfig);
-router.post('/broadcast', broadcastMail);
-router.get('/report', generateReport);
-
-router.get('/institutes', getInstitutes);
-router.post('/institutes', createInstitute);
-router.delete('/institutes/:id', deleteInstitute);
+router.get('/', getAwards);
 
 export default router;
